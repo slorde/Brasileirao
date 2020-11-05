b8bcb112a15de74f15738a0607fc9cd9f446fd5c
Gabriel Silva <slorde2@gmail.com>
2020-11-05 19:52:35 +0000

    tratando cors

--- src/main/java/com/fsoft/brasileirao/controller/CompeticaoController.java
+++ src/main/java/com/fsoft/brasileirao/controller/CompeticaoController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.fsoft.brasileirao.dto.ResultadoDTO;
 import com.fsoft.brasileirao.service.CompeticaoService;
 import com.fsoft.brasileirao.service.ResultadoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("competicoes")
 public class CompeticaoController {
--- src/main/java/com/fsoft/brasileirao/controller/ResultadoController.java
+++ src/main/java/com/fsoft/brasileirao/controller/ResultadoController.java
@@ -2,12 +2,14 @@ package com.fsoft.brasileirao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fsoft.brasileirao.service.ResultadoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("resultados")
 public class ResultadoController {
