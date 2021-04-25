3913bd85f90f2b28dca378190f90d7af91dea956
Gabriel <slorde2@gmail.com>
2021-04-25 17:33:01 -0300

    ignoring unknow

--- src/main/java/com/fsoft/brasileirao/service/mapping/ResultadoMapping.java
+++ src/main/java/com/fsoft/brasileirao/service/mapping/ResultadoMapping.java
@@ -2,9 +2,12 @@ package com.fsoft.brasileirao.service.mapping;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResultadoMapping implements Serializable{
 
 	private static final long serialVersionUID = 1L;
